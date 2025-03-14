626aebd207d2c05332305d9ea6266f5401647bbb
Mouadh <mouadhrached0125@gmail.com>
2025-03-14 10:08:51 +0100

    Nouveau message

--- script.py
+++ script.py
@@ -1,1 +1,7 @@
 print("Bienvenue dans mon premier programme python avec Git")x
+multiplication
+def soustraction(a, b):
+    return a - b
+
+resultat = soustraction(8, 3)
+print(f"Le résultat de la soustraction est : {resultat}")
